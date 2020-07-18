04a87a8a06c81480f46e3e1ea29841b879fd122f
garapati likhith <likhithgarapati2002@gmail.com>
2020-07-18 23:47:48 +0530

    Update settings.py

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fsharedatabase',
+        'USER': 'likhith',
+        'PASSWORD': 'Karthikeya@15421',
+        'HOST': 'fshareserver1.database.windows.net',
+        'PORT': '1433',
     }
 }
 
